eef565ce6782ebf0d63e0ae60cb8477870c0876d
samkimpepper <vegielcl@gmail.com>
2023-08-03 17:11:25 +0900

    fix slide2

--- home.js
+++ home.js
@@ -308,7 +308,7 @@ tags.forEach(tag => {
 
 // 검색기능
 async function search(searchText) {
-    
+    searchText = searchText.toLowerCase();
     let videoList = await getVideoList();
     let videoTags = new Set();
     videoList.forEach(video => video.video_tag.forEach(tag => videoTags.add(tag)));
