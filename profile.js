b3e3d6c4cd8b43c32a5bc95f1170fa20e6efc3d3
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-08-07 16:44:01 +0900

    feat: add notification

--- profile.js
+++ profile.js
@@ -1,4 +1,5 @@
 const userProfile = document.querySelector('.nav-profile-pic > .user-avatar');
+const notification = document.querySelector('.nav-profile-notifications');
 
 userProfile.addEventListener("click", () => {
     const profileList = document.querySelector('.nav-profile-pic-list');
@@ -10,15 +11,99 @@ userProfile.addEventListener("click", () => {
     }
 });
 
+function getNotiContent() {
+    let notiContent = document.querySelector('.notiContent');
+    let notificationList = JSON.parse(localStorage.getItem('notification')) || [];
+    const comment = JSON.parse(localStorage.getItem('comment')) || [];
+    let userName = getUserProfile().get('userName');
+    let notificationCount = 0;
+
+    comment.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+    notificationList.forEach((noti) => {
+      if (noti[0] === userName) {
+        notificationCount = noti[1].notification;
+      }
+    });
+
+    let old = document.querySelector('.noNoti');
+    if (notificationCount == 0) {
+        let newComment = document.querySelectorAll('.notificationContents');
+        for (let i = 0; i < newComment.length; i++) {
+            notiContent.removeChild(newComment[i]);
+        }
+        old.style.display = 'flex';
+    } else {
+        old.style.display = 'none';
+        
+        for (let i = 0; i < notificationCount; i++) {
+            let newComment = document.createElement('div');
+            newComment.setAttribute('class', 'notificationContents');
+            let content = '';
+            if (userName !== comment[i].userName) {
+                let videoURL = `location.href='../html/video.html?id=${comment[i].id}'`;
+                newComment.setAttribute('onclick', videoURL);
+                content += `
+                    <img
+                        src="${comment[i].userProfile}"
+                        class="user-avatar" alt="${comment[i].userProfile} avatar" title="avatar"
+                    />
+                    <p>${comment[i].comment}</p>
+                    <p>${timeForToday(comment[i].time)}</p>
+                `;
+            }
+            newComment.innerHTML = content;
+            notiContent.prepend(newComment);
+        }
+    }
+
+    const navNotification = document.querySelector('.nav-profile-notifications');
+    const notificationIcon = document.querySelector('.notificationCount');
+
+    navNotification.addEventListener("click", () => {
+        notificationList.forEach((noti) => {
+            if (noti[0] === userName) {
+                noti[1].notification = 0; 
+                noti[1].isRead = true; 
+                notificationIcon.style.display = 'none';  
+            }
+        });
+        localStorage.setItem('notification', JSON.stringify(notificationList));
+    });
+}
+
+notification.addEventListener("click", () => {
+    const notiList = document.querySelector('.notificationList');
+    const notiCount = document.querySelector('.notificationCount');
+    notiList.classList.toggle('active');
+    if (notiList.classList.contains('active')) {
+        notiList.style.display = 'flex';
+        getNotiContent();
+    } else {
+        notiList.style.display = 'none';
+    }
+    notiCount.style.display = 'none';
+});
+
 let isProfileListVisible = true;
 document.addEventListener('click', (event) => {
     const profileList = document.querySelector('.nav-profile-pic-list');
-    const profileImg = document.querySelector('.nav-profile-pic-list img');
     isProfileListVisible = !isProfileListVisible;
     if (isProfileListVisible && !profileList.contains(event.target)) {
         profileList.classList.remove('active');
         profileList.style.display = 'none';
         profileList.style.transition = 'transition: width 0.3s ease, transform 0.3s ease';
+    }
+});
+
+let isNotificationListVisible = true;
+document.addEventListener('click', (event) => {
+    const nofiList = document.querySelector('.notificationList');
+    isNotificationListVisible = !isNotificationListVisible;
+    if (isNotificationListVisible && !nofiList.contains(event.target)) {
+        nofiList.classList.remove('active');
+        nofiList.style.display = 'none';
+        nofiList.style.transition = 'transition: width 0.3s ease, transform 0.3s ease';
     }
 });
 
@@ -61,8 +146,31 @@ function setProfile(profile) {
     localStorage.setItem('user', JSON.stringify(Array.from(userData.entries())));
 }
 
+function setNotification() {
+    const notificationData = localStorage.getItem('notification');
+    if (notificationData === null) {
+        let oreumi = {
+            "notification": 0,
+            "videoId": [],
+            "isRead": false
+        };
+        let gaejo = {
+            "notification": 0,
+            "videoId": [],
+            "isRead": false
+        };
+    
+        let notiData = new Map();
+        notiData.set('Oreumi', oreumi);
+        notiData.set('Gaejo', gaejo);
+    
+        localStorage.setItem('notification', JSON.stringify(Array.from(notiData.entries())));
+    }
+}
+window.onload = setNotification();
+
 function updateProfile() {
-    let commentProfile = document.querySelector('.comment-input-area > .profile-pic > img');
+    const commentProfile = document.querySelector('.comment-input-area > .profile-pic > img');
     let navProfile = document.querySelector('.nav-profile-pic > img')
     const userData = getUserProfile();
   
@@ -79,12 +187,56 @@ function updateProfile() {
 }
 window.onload = updateProfile();
 
+function notificationSend() {
+    const userData = getUserProfile();
+    let notificationList = JSON.parse(localStorage.getItem('notification')) || [];
+    const navNotification = document.querySelector('.nav-profile-notifications');
+    const notificationIcon = document.querySelector('.notificationCount');
+
+    navNotification.addEventListener("click", () => {
+        notificationList.forEach((noti) => {
+            if (noti[0] === userData.get('userName')) {
+                noti[1].notification = 0; 
+                noti[1].isRead = true; 
+                notificationIcon.style.display = 'none';  
+            }
+        });
+        localStorage.setItem('notification', JSON.stringify(notificationList));
+    });
+
+}
+
+function updateNotificationProfilePick() {
+    const userData = getUserProfile();
+    const notificationList = JSON.parse(localStorage.getItem('notification')) || [];
+    let notificationIcon = document.querySelector('.notificationCount');
+    let notificationIconText = document.querySelector('.notificationCount > span');
+
+    notificationList.forEach((noti) => {
+        if (noti[0] === userData.get('userName')) {
+            if (noti[1].isRead) {
+                notificationIcon.style.display = 'none';
+            } else {
+                if (noti[1].notification > 0) {
+                    notificationIcon.style.display = 'flex';
+                    notificationIconText.innerText = noti[1].notification;
+                } else {
+                    notificationIcon.style.display = 'none';
+                }
+            }
+            notificationSend();
+        }
+    });
+}
+window.onload = updateNotificationProfilePick();
+
 const profileCandidateList = document.querySelectorAll(".nav-profile-pic-list > div");
 profileCandidateList.forEach((profile) => {
     profile.addEventListener("click", (event) => {
         const profileList = document.querySelector('.nav-profile-pic-list');
         setProfile(profile);
         updateProfile();
+        updateNotificationProfilePick();
         if (profile.contains(event.target)) {
             profileList.classList.remove('active');
             profileList.style.display = 'none';
@@ -92,4 +244,3 @@ profileCandidateList.forEach((profile) => {
         }
     });
 });
-
